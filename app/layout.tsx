224b8b9b8db319f7a43a141950ca0d7682da0d3a
opsora-agency <opsoraagency@gmail.com>
2026-03-05 17:00:34 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "node_modules/react-modal-video/css/modal-video.css";
 import "../styles/index.css";
 import ChatBot from '@/components/ChatBot/ChatBot';
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import GoogleAnalytics from './components/GoogleAnalytics'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
           <Footer />
           <ScrollToTop />
           <ChatBot />
+          <GoogleAnalytics />
         </Providers>
       </body>
     </html>
